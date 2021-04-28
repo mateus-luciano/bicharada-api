0c6b5d08e43f1333a913e464b3f68020f918e6b4
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 10:38:50 -0300

    created user middleware

---
+++ src/app/middlewares/user.js
@@ -0,0 +1,47 @@
+import User from '../models/User';
+
+function validateData(req, res, next) {
+  const { email, password, firstName, lastName } = req.body;
+
+  if (!email || !password || !firstName || !lastName) {
+    return res.status(400).json({
+      message: 'Dados inválidos',
+    });
+  }
+
+  next();
+}
+
+async function checkEmail(req, res, next) {
+  const { email } = req.body;
+
+  const user = await User.findOne({
+    where: { email },
+  });
+
+  if (user) {
+    return res.status(409).json({
+      message: 'E-mail já registrado.',
+    });
+  }
+
+  next();
+}
+
+async function validateUserExists(req, res, next) {
+  const { uid } = req.params;
+
+  const user = await User.findOne({
+    where: { uid },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'Não encontrado',
+    });
+  }
+
+  next();
+}
+
+export { validateData, checkEmail, validateUserExists };
--- src/app/routers/users.js
+++ src/app/routers/users.js
@@ -1,12 +1,22 @@
 import { Router } from 'express';
 import UserControler from '../controllers/UserController';
+import {
+  validateData,
+  checkEmail,
+  validateUserExists,
+} from '../middlewares/user';
 
 const routes = new Router();
 
 routes.get('/users', UserControler.index);
-routes.get('/users/:uid', UserControler.show);
-routes.post('/users', UserControler.store);
-routes.put('/users/:uid', UserControler.update);
-routes.delete('/users/:uid', UserControler.delete);
+routes.get('/users/:uid', validateUserExists, UserControler.show);
+routes.post('/users', checkEmail, validateData, UserControler.store);
+routes.put(
+  '/users/:uid',
+  validateUserExists,
+  validateData,
+  UserControler.update
+);
+routes.delete('/users/:uid', validateUserExists, UserControler.delete);
 
 export default routes;
