cac0da68b5ca354fb3ef2bd7016ce4cafbf1e5a5
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 10:28:28 -0300

    updated user routes

--- src/app/routers/users.js
+++ src/app/routers/users.js
@@ -4,6 +4,9 @@ import UserControler from '../controllers/UserController';
 const routes = new Router();
 
 routes.get('/users', UserControler.index);
+routes.get('/users/:uid', UserControler.show);
 routes.post('/users', UserControler.store);
+routes.put('/users/:uid', UserControler.update);
+routes.delete('/users/:uid', UserControler.delete);
 
 export default routes;
