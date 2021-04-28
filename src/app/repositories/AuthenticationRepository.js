03a1fdda96049e18271f705a04b87a3ba636defb
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 10:44:20 -0300

    created authentication repository

---
+++ src/app/repositories/AuthenticationRepository.js
@@ -0,0 +1,86 @@
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import mailer from '../../config/mailer';
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class AuthenticationRepository {
+  async loginAuthentication(email) {
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    // eslint-disable-next-line camelcase
+    const { uid, first_name } = user;
+
+    return {
+      user: {
+        uid,
+        email,
+        first_name,
+      },
+      token: jwt.sign({ uid }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    };
+  }
+
+  async forgotPassword(email) {
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    const token = crypto.randomBytes(4).toString('hex');
+
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+
+    await User.update(
+      {
+        password_reset_token: token,
+        password_reset_expires: now,
+      },
+      {
+        where: {
+          uid: user.uid,
+        },
+        returning: true,
+      }
+    );
+
+    mailer.sendMail(
+      {
+        to: email,
+        from: 'teeusdm@gmail.com',
+        html: `<p>Voce esqueceu sua senha? utilize o token { ${token} }, para recuperar.</p>`,
+      },
+      (error) => {
+        if (error) {
+          return { error: 'Não foi possivel enviar o email de recuperação' };
+        }
+      }
+    );
+
+    return { message: 'Mensagem enviada' };
+  }
+
+  async resetPassword(body) {
+    const { email, password } = body;
+
+    const response = await User.update(
+      { password },
+      {
+        where: { email },
+        returning: true,
+      }
+    );
+
+    return response;
+  }
+}
+
+export default new AuthenticationRepository();
