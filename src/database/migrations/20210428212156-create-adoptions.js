1d7b420d487a57509c1090594ec23dc9227bc0fe
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 18:27:18 -0300

    created adoptions migration

---
+++ src/database/migrations/20210428212156-create-adoptions.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('adoptions', {
+      uid: {
+        type: Sequelize.DataTypes.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.DataTypes.STRING(60),
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.DataTypes.STRING(460),
+        allowNull: false,
+        unique: true,
+      },
+      address: {
+        type: Sequelize.DataTypes.STRING(160),
+        allowNull: false,
+        unique: true,
+      },
+      type: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      user_uid: {
+        type: Sequelize.DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'uid',
+        },
+      },
+      created_at: { type: Sequelize.DATE, allowNull: false },
+      updated_at: { type: Sequelize.DATE, allowNull: false },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('adoptions');
+  },
+};
