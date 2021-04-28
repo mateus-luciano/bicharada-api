f90289f90075e90dc03a60a407b22db5ecff12be
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 09:40:13 -0300

    initial commit

---
+++ src/app/routers/swagger.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocumention from '../../swagger-documention.json';
+
+const routes = new Router();
+
+routes.use(
+  '/documention',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocumention),
+);
+
+export default routes;
