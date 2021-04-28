c701f5f5c1edfd01ae99b1d752b4139fb9258ae3
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 20:49:08 -0300

    created attachment model

---
+++ src/app/models/Attachment.js
@@ -0,0 +1,55 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Attachment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        uid: {
+          type: Sequelize.DataTypes.UUID,
+          allowNull: false,
+          defaultValue: Sequelize.DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        file: {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+        },
+        url: {
+          type: Sequelize.DataTypes.VIRTUAL,
+          get() {
+            return `http://localhost:3333/attachments/${this.file}`;
+          },
+        },
+        name: {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+        },
+        adoption_uid: {
+          type: Sequelize.DataTypes.UUID,
+          allowNull: false,
+          references: {
+            model: 'adoptions',
+            key: 'uid',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+      },
+      {
+        sequelize,
+        tableName: 'attachments',
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Adoption, {
+      as: 'attachment',
+      foreignKey: 'adoption_uid',
+    });
+  }
+}
+
+export default Attachment;
