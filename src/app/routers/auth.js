11a806e33ebe747a81e6c13f23d6fb06fd856c8a
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 11:03:47 -0300

    created auth routes

---
+++ src/app/routers/auth.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import AuthController from '../controllers/AuthController';
+import { checkUserExists, checkPassword } from '../middlewares/authLogin';
+import {
+  validateData,
+  validateDataToken,
+  validateToken,
+} from '../middlewares/resetPassword';
+
+const routes = new Router();
+
+routes.post(
+  '/login',
+  checkCustomerExists,
+  checkPassword,
+  AuthController.login
+);
+routes.post(
+  '/forgot-password',
+  validateData,
+  checkUserExists,
+  AuthController.forgotPassword
+);
+routes.post(
+  '/reset-password',
+  validateData,
+  validateDataToken,
+  validateToken,
+  AuthController.resetPassword
+);
+
+export default routes;
