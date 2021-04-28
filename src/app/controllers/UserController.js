cef91c8f8f62667050375df30aa3a636ec52eb65
Mateus Luciano <teeusdm@gmail.com>
2021-04-28 10:27:42 -0300

    add find, update, delete in user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,11 +13,47 @@ class UserController {
     }
   }
 
+  async show(req, res) {
+    const { uid } = req.params;
+
+    try {
+      const data = await UserRepository.find(uid);
+
+      return res.json(data);
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
+
   async store(req, res) {
     try {
       const data = await UserRepository.save(req.body);
 
       return res.status(201).json(data);
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
+
+  async update(req, res) {
+    const { uid } = req.params;
+
+    try {
+      const data = await UserRepository.update(req.body, uid);
+
+      return res.json(data);
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
+
+  async delete(req, res) {
+    const { uid } = req.params;
+
+    try {
+      await UserRepository.remove(uid);
+
+      return res.sendStatus(204);
     } catch (error) {
       return res.status(error.status || 400).json(error);
     }
