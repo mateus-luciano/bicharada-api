f92b43aa948a3197ce926e3ba4c17e45235b8d9a
Mateus Luciano <teeusdm@gmail.com>
2021-05-05 12:26:28 -0300

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,36 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
 module.exports = {
+  // Stop running tests after `n` failures
   bail: 1,
+
+  // Automatically clear mock calls and instances between every test
   clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['./src/app/**/*.js'],
+
+  // The directory where Jest should output its coverage files
   coverageDirectory: './coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
+
+  // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text', 'lcov'],
+
+  // The test environment that will be used for testing
   testEnvironment: 'node',
+
+  // The glob patterns Jest uses to detect test files
   testMatch: ['**/__tests__/**/*.test.js?(x)'],
+
+  // A map from regular expressions to paths to transformers
   transform: { '.(js|jsx|ts|tsx)': '@sucrase/jest-plugin' },
 };
