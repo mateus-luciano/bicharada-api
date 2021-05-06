c1c63641edcf71a4e4dbadbede77afdb5c690d49
Mateus Luciano <teeusdm@gmail.com>
2021-05-06 08:51:11 -0300

    installed dependencies

--- src/app/routers/swagger.js
+++ src/app/routers/swagger.js
@@ -1,14 +1,13 @@
-// import { Router } from 'express';
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import swaggerUi from 'swagger-ui-express';
-// import swaggerDocumention from '../../swagger-documention.json';
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocumention from '../../swagger-documention.json';
 
-// const routes = new Router();
+const routes = new Router();
 
-// routes.use(
-//   '/documentation',
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerDocumention)
-// );
+routes.use(
+  '/documentation',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocumention)
+);
 
-// export default routes;
+export default routes;
