aa4c0d74cc0d8bf197fafda458517a8821613c83
Mateus Luciano <teeusdm@gmail.com>
2021-05-07 21:32:57 -0300

    updated user documention

--- src/app/documentation/user.js
+++ src/app/documentation/user.js
@@ -1,23 +1,8 @@
-exports.User = {
-  uid: '17a3ef3a-2c78-446e-a7eb-71c65adca270',
-  email: 'mateus@example.com',
-  name: 'Mateus Luciano da Silva',
-  city: 'Parobé',
-  phone: '45 991434191',
-};
-
-exports.UserStore = {
-  email: 'mateus@example.com',
-  password: 'mateus123',
-  name: 'Mateus Luciano da Silva',
-  city: 'Parobé',
-  phone: '45 991434191',
-};
-
-exports.UserUpdate = {
-  email: 'mateus@example.com',
-  password: 'mateus123',
-  name: 'Mateus Luciano da Silva',
-  city: 'Novo Hamburgo',
-  phone: '45 999999999',
+exports.exports = {
+  uid: 'UUID do usuário',
+  email: 'E-mail do usuário',
+  name: 'Nome do usuário',
+  city: 'Cidade do usuário',
+  phone: 'Número do celular do usuário',
+  region: 'UUID da região do usuário',
 };
