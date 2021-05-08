6e84a1b22fa7492a440e112fc466fb092af1d503
Mateus Luciano <teeusdm@gmail.com>
2021-05-07 22:14:57 -0300

    user services - add cache in get, show, updated

--- src/app/services/User.js
+++ src/app/services/User.js
@@ -1,25 +1,45 @@
 import UserRepository from '../repositories/User';
-// import Cache from '../utils/Cache';
+import Cache from '../../lib/Cache';
 
 class UserService {
   async getAll(limit, page) {
-    // const cache = await Cache.get(`users-${page}`);
+    const usersCache = await Cache.get(`users--${page}`);
 
-    // if (cache !== null) {
-    //   return JSON.parse(cache);
-    // }
+    if (usersCache !== null) {
+      return {
+        data: JSON.parse(usersCache),
+        cache: true,
+      };
+    }
 
     const data = await UserRepository.getAll(limit, page);
 
-    // await Cache.set(`users-${page}`, JSON.stringify(data));
+    await Cache.setExpire(`users--${page}`, JSON.stringify(data), 3600);
 
-    return data;
+    return {
+      data,
+      cache: false,
+    };
   }
 
   async find(uid) {
+    const userCache = await Cache.get(`user-${uid}`);
+
+    if (userCache !== null) {
+      return {
+        data: JSON.parse(userCache),
+        cache: true,
+      };
+    }
+
     const data = await UserRepository.find(uid);
 
-    return data;
+    await Cache.setExpire(`user-${uid}`, JSON.stringify(data), 3600);
+
+    return {
+      data,
+      cache: false,
+    };
   }
 
   async save(email, password, name, city, phone, region) {
@@ -46,11 +66,17 @@ class UserService {
       uid
     );
 
-    return data;
+    await Cache.setExpire(`user-${uid}`, JSON.stringify(data), 3600);
+
+    return {
+      data,
+      cache: false,
+    };
   }
 
   async remove(uid) {
     await UserRepository.remove(uid);
+    await Cache.delete(`user-${uid}`);
   }
 }
 
